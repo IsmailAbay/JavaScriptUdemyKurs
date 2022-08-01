803ca3d4461332bdcb61b802ae5f04bc8f1772ed
ismailabay <ismailabay.dev@gmail.com>
2022-08-01 20:12:22 +0300

    operatörler

---
+++ UdemyDers2/app.js
@@ -0,0 +1,25 @@
+let sonuç;
+let a = 10, b = 20, c = 30;
+
+// 1- Aritmetik Operatörler
+sonuç = a + b;
+sonuç = a - b;
+sonuç = a * b;
+sonuç = a / b;
+sonuç = a % b;
+// sonuç = a++;
+// sonuç = ++a;
+// sonuç = --a;
+// sonuç = a--;
+
+// 2- Atama Operatörleri
+sonuç = a;
+sonuç += a;
+sonuç -= a;
+sonuç *= a;
+sonuç /= a;
+sonuç %= a;
+// 3- karşılaştırma Operatörleri
+
+// 4- mantıksal Operatörler
+console.log(sonuç)
