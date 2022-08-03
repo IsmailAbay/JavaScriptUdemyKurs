c0cadaa92db9f663f6427575a9a5fdec52a06342
ismailabay <ismailabay.dev@gmail.com>
2022-08-03 22:55:45 +0300

    if - else

---
+++ UdemyDers3/app.js
@@ -0,0 +1,15 @@
+let username= "ismailabay"
+let password=1313
+
+let isloggedin = true;
+
+if (isloggedin) {
+    console.log("uygulamaya giriş yapıldı")
+}
+
+else {
+console.log("merhaba")
+}
+
+
+
