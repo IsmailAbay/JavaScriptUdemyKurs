2075bc159d001381db90d68f1c514eafa04521a9
ismailabay <ismailabay.dev@gmail.com>
2022-08-04 13:41:57 +0300

    if-else

---
+++ UdemyDers4/app.js
@@ -0,0 +1,12 @@
+// yas >= 18 
+// mezuniyet == lise yada == üniversite
+let mezuniyet = "lise"
+let yas = 20;
+
+ if ((yas >= 18) && (mezuniyet == "lise"|| mezuniyet == üniversite)){
+     console.log("ehliyet alabilirsiniz")
+ } 
+ else {
+    console.log("ehliyet şartları tutmuyor")
+
+}
