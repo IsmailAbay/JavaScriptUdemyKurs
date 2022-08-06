37f671e227fe583aac06800bd1ae22e59d190489
ismailabay <ismailabay.dev@gmail.com>
2022-08-06 19:29:53 +0300

    Numbers

---
+++ UdemyDers9/app.js
@@ -0,0 +1,28 @@
+let sonuc
+
+sonuc = 10
+sonuc = "10"
+sonuc = Number("10")
+sonuc = parseInt("10.6")
+sonuc = parseFloat("10a")
+sonuc = parseFloat("a10")
+
+sonuc = isNaN("10")
+
+let sayi = 15.12357469
+   
+sonuc = sayi .toPrecision(5)
+
+sonuc = sayi.toFixed(5)
+
+sonuc = Math.round(2.4)
+sonuc = Math.round(2.6)
+sonuc = Math.ceil(2.2)
+sonuc = Math.floor(26)
+sonuc = Math.sqrt(2.6)
+sonuc = Math.sqrt(2,3)
+
+
+
+console.log(typeof sonuc)
+console.log(sonuc)
