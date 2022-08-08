4cb48b58d57bc3cbd15a38a6395fc5ca2bd62db1
ismailabay <ismailabay.dev@gmail.com>
2022-08-08 20:20:19 +0300

    ders

---
+++ UdemyDers10/app.js
@@ -0,0 +1,11 @@
+let simdi = new Date () // şimdiki tarih - saat
+
+// Get Methods
+sonuc = simdi
+sonuc = simdi.getDate()
+// sonuc = simdi.getDay()
+// sonuc = simdi.getFullYear()
+// sonuc = simdi.getHours()
+// sonuc = simdi.getTime()
+
+console.log(sonuc)
